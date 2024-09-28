3992351eabbe263a8c369369de8b1baf347804ce
drebo <drebo2324@gmail.com>
2024-09-28 16:34:12 -0400

    Docs

--- src/main/java/com/drebo/microservices/gateway/config/SecurityConfig.java
+++ src/main/java/com/drebo/microservices/gateway/config/SecurityConfig.java
@@ -9,9 +9,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    //endpoints excluded from authorization
+    private final String[] freeResourceUrls = {
+            "/swagger-ui.html",
+            "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/api-docs/**",
+            "/aggregate/**"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                //authorization rules for incoming requests
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        //Allow all resource urls
+                        .requestMatchers(freeResourceUrls).permitAll()
+                        //All request must be authorized
+                        .anyRequest().authenticated())
+                //server expects requests to have valid JWT for authentication
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
