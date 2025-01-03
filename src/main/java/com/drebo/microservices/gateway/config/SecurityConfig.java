c06525641ce44014206fe45e3fe5692c5973c35d
dreBo <drebo2324@gmail.com>
2025-01-03 19:34:13 -0300

    distributed tracing / grafana datasource

--- src/main/java/com/drebo/microservices/gateway/config/SecurityConfig.java
+++ src/main/java/com/drebo/microservices/gateway/config/SecurityConfig.java
@@ -22,7 +22,8 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-resources/**",
             "/api-docs/**",
-            "/aggregate/**"
+            "/aggregate/**",
+            "/actuator/prometheus"
     };
 
     @Bean
